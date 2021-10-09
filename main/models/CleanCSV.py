bb72c0bbbc2d363f6935240fad5512025066d8d6
prajwalkrishna2@gmail.com <prajwalkrishna2@gmail.com>
2021-10-09 14:29:36 -0400

    demojize has been fixed, all the emojies like emoticons, symbols, maps etc., have a ptterm and everything is removed

--- main/models/CleanCSV.py
+++ main/models/CleanCSV.py
@@ -42,7 +42,12 @@ def demoji(text):
         u"\U0001F600-\U0001F64F"  # emoticons
         u"\U0001F300-\U0001F5FF"  # symbols & pictographs
         u"\U0001F680-\U0001F6FF"  # transport & map symbols
-
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        u"\U00010000-\U0010ffff"
+                               "]+", flags=re.UNICODE)
+    return(emoji_pattern.sub(r'', text))
 
 
 def main():
