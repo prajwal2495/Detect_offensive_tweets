0356db0a9f5d1624acede286e8ff401ce878b8e6
MrinalChaudhari <mission999c@gmail.com>
2021-10-13 22:04:34 -0400

    loaded testing file

--- Experiments-Mrinal/deep_learning_models.py
+++ Experiments-Mrinal/deep_learning_models.py
@@ -9,5 +9,9 @@ def main():
     train_data = read_data(train_filename)
     train_data = train_data[['Tweet', 'Class']]
 
+    test_filename = './Dataset/Marathi_Test.csv'
+    test_data = read_data(test_filename)
+    test_data = test_data[['Tweet', 'Class']]
+    
 if __name__ == '__main__':
     main()
