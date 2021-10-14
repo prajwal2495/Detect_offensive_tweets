54564da58f2b367b202dfbf193c77260e82af73b
MrinalChaudhari <mission999c@gmail.com>
2021-10-13 22:11:30 -0400

    MLP classifier model fit and predict on training and testing data

--- Experiments-Mrinal/deep_learning_models.py
+++ Experiments-Mrinal/deep_learning_models.py
@@ -8,7 +8,13 @@ def read_data(filename):
 
 def TFIDF_Multi_Layer_Perceptron(train_data, test_data):
     model = make_pipeline(TfidfVectorizer(),MLPClassifier(random_state=1, max_iter=100, learning_rate_init=0.001, activation='logistic'))
+    X_train = train_data['Tweet']
+    y_train = train_data['Class']
 
+    X_test = test_data['Tweet']
+    y_test = test_data['Class']
+    model.fit(X_train, y_train)
+    labels = model.predict(X_test)
 
 def main():
     train_filename = './Dataset/Marathi_Train.csv'
