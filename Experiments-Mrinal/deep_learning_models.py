dd03a1e04bff9a71aa58c729d641f22da97bb0a4
MrinalChaudhari <mission999c@gmail.com>
2021-10-13 22:06:06 -0400

    Method for MLP classifer with training and testing data parameters

--- Experiments-Mrinal/deep_learning_models.py
+++ Experiments-Mrinal/deep_learning_models.py
@@ -4,6 +4,8 @@ def read_data(filename):
     file_content = pd.read_csv(filename)
     return file_content
 
+def TFIDF_Multi_Layer_Perceptron(train_data, test_data):
+
 def main():
     train_filename = './Dataset/Marathi_Train.csv'
     train_data = read_data(train_filename)
@@ -12,6 +14,6 @@ def main():
     test_filename = './Dataset/Marathi_Test.csv'
     test_data = read_data(test_filename)
     test_data = test_data[['Tweet', 'Class']]
-    
+
 if __name__ == '__main__':
     main()
