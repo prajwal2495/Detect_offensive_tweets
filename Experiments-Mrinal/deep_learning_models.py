40cce73f950b1d25473b61ead058193f7aff8e9c
MrinalChaudhari <mission999c@gmail.com>
2021-10-13 21:04:09 -0400

    main method implemented and loaded training data in main function

--- Experiments-Mrinal/deep_learning_models.py
+++ Experiments-Mrinal/deep_learning_models.py
@@ -3,3 +3,11 @@ import pandas as pd
 def read_data(filename):
     file_content = pd.read_csv(filename)
     return file_content
+
+def main():
+    train_filename = './Dataset/Marathi_Train.csv'
+    train_data = read_data(train_filename)
+    train_data = train_data[['Tweet', 'Class']]
+
+if __name__ == '__main__':
+    main()
