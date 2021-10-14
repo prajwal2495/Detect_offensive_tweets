398102736ee09c80955283e22944a038b68cfa05
MrinalChaudhari <mission999c@gmail.com>
2021-10-13 22:21:32 -0400

    MLP model has been implemented

--- Experiments-Mrinal/deep_learning_models.py
+++ Experiments-Mrinal/deep_learning_models.py
@@ -15,30 +15,36 @@ def read_data(filename):
 
 
 def TFIDF_Multi_Layer_Perceptron(train_data, test_data):
+    print("TFIDF + MultiLayer Perceptron")
     model = make_pipeline(TfidfVectorizer(),MLPClassifier(random_state=1, max_iter=100, learning_rate_init=0.001, activation='logistic'))
     X_train = train_data['Tweet']
     y_train = train_data['Class']
 
     X_test = test_data['Tweet']
     y_test = test_data['Class']
+
     model.fit(X_train, y_train)
     labels = model.predict(X_test)
+
     print("Accuracy:", metrics.accuracy_score(y_test, labels) * 100)
+    # takes a lot of time to generate 10 trees and find accuracy
     cm = confusion_matrix(y_test, labels, train_data['Class'].unique())
     print("Confusion matrix", cm)
+    
     print(classification_report(y_test, labels, digits=4))
     print("\n\n")
 
 
 def main():
-    train_filename = './Dataset/Marathi_Train.csv'
+    train_filename = './Data/Marathi_Train.csv'
     train_data = read_data(train_filename)
     train_data = train_data[['Tweet', 'Class']]
 
-    test_filename = './Dataset/Marathi_Test.csv'
+    test_filename = './Data/Marathi_Test.csv'
     test_data = read_data(test_filename)
     test_data = test_data[['Tweet', 'Class']]
 
+    TFIDF_Multi_Layer_Perceptron(train_data, test_data)
 
 if __name__ == '__main__':
     main()
