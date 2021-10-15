a273aceb03650d8dc9c52a8e1b5828bf8c765c0e
ShrunaliPaygude <shrunali.paygude@gmail.com>
2021-10-15 17:46:55 -0400

    implemented LSTM with softmax using dense and printing model summary

--- Experiments - Shrunali/LSTM.py
+++ Experiments - Shrunali/LSTM.py
@@ -2,6 +2,12 @@ import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
 from keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.layers import Embedding
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dropout
 
 def read_data(filename):
     file_content = pd.read_csv(filename)
@@ -14,4 +20,22 @@ X_train, X_valid, Y_train, Y_valid = train_data.Tweet, test_data.Tweet, train_da
 
 tokenizer = Tokenizer(nb_words=2500, lower=True,split=' ')
 tokenizer.fit_on_texts(train_data['Tweet'].values)
-print(tokenizer.word_index)  # To see the dictionary
+#print(tokenizer.word_index)  # To see the dictionary
+X = tokenizer.texts_to_sequences(train_data['Tweet'].values)
+X = pad_sequences(X)
+
+Y = pd.get_dummies(train_data['Class']).values
+X_valid = tokenizer.texts_to_sequences(test_data['Tweet'].values)
+X_valid = pad_sequences(X_valid)
+Y_valid = pd.get_dummies(test_data['Class']).values
+
+embed_dim = 128
+lstm_out = 256
+batch_size = 16
+model = Sequential()
+model.add(Embedding(3000, embed_dim,input_length = X.shape[1]))
+model.add(LSTM(lstm_out))
+model.add(Dropout(0.2))
+model.add(Dense(2,activation='softmax'))
+model.compile(loss = 'binary_crossentropy', optimizer='adam',metrics = ['accuracy'])
+print(model.summary())
