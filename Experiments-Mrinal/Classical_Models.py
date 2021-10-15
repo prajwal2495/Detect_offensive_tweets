c3c74857d135df559ed8bbf4de6ab88594487c6e
MrinalChaudhari <mission999c@gmail.com>
2021-10-15 11:03:15 -0400

    method for K-nearest neighbors classifier and imported necessary libraries from sklearn

--- Experiments-Mrinal/Classical_Models.py
+++ Experiments-Mrinal/Classical_Models.py
@@ -1,8 +1,12 @@
 import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 
 def read_data(filename):
     file_content = pd.read_csv(filename)
     return file_content
+
+def TFIDF_KNN(train_data, test_data):
 
 def main():
     train_filename = './Data/Marathi_Train.csv'
@@ -12,3 +16,5 @@ def main():
     test_filename = './Data/Marathi_Test.csv'
     test_data = read_data(test_filename)
     test_data = test_data[['Tweet', 'Class']]
+
+    TFIDF_KNN(train_data, test_data)
