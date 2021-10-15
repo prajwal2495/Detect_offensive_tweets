b7dea5ec79a93580c6ea17420cb06957e9005c4a
MrinalChaudhari <mission999c@gmail.com>
2021-10-15 10:17:42 -0400

    iterating through files and appending file contents to dictionary

--- Experiments-Mrinal/Inter_Annotator_Agreement.py
+++ Experiments-Mrinal/Inter_Annotator_Agreement.py
@@ -5,5 +5,10 @@ from sklearn.metrics import cohen_kappa_score
 def main():
     files = ['./Inter_Annotator_Data/Mayuresh_mrinal.xlsx', './Inter_Annotator_Data/mrinal.xlsx']
 
+    file_contents = []
+    for file in files:
+        file_content = pd.read_excel(file)
+        file_contents.append(file_content)
+
 if __name__ == '__main__':
     main()
