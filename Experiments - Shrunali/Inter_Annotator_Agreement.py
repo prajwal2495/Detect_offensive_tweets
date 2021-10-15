47382fb5ef57543db65f4b178501f33285c4f901
ShrunaliPaygude <shrunali.paygude@gmail.com>
2021-10-15 15:38:58 -0400

    inter- annotator agreement from Mrinal, implementation of COhen

---
+++ Experiments - Shrunali/Inter_Annotator_Agreement.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+from sklearn.metrics import cohen_kappa_score
+
+def main():
+    files = ['./Inter_Annotator_Data/Mayuresh_mrinal.xlsx', './Inter_Annotator_Data/mrinal.xlsx']
+
+    file_contents = []
+    for file in files:
+        file_content = pd.read_excel(file)
+        file_contents.append(file_content)
+
+        agreements = []
+    for annotator in range(0, len(file_contents), 2):
+        agreement = cohen_kappa_score((file_contents[annotator]['Class'][:100]), (file_contents[annotator + 1]['Class'][:100]))
+        agreements.append(agreement)
+
+        print(agreement)
+
+def main2():
+    files = ['./Inter_Annotator_Data/Mayuresh_mrinal.xlsx', './Inter_Annotator_Data/mrinal.xlsx']
+
+
+if __name__ == '__main__':
+    main()
