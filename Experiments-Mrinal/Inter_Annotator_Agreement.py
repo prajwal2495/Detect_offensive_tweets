f45bfa3a9638e759fd1119dc0c332210f4fd4515
MrinalChaudhari <mission999c@gmail.com>
2021-10-15 10:11:54 -0400

    import cohen kappa from sklearn and loaded annotator files

---
+++ Experiments-Mrinal/Inter_Annotator_Agreement.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+from sklearn.metrics import cohen_kappa_score
+
+def main():
+    files = ['./Inter_Annotator_Data/Mayuresh_mrinal.xlsx', './Inter_Annotator_Data/mrinal.xlsx']
+
+if __name__ == '__main__':
+    main()
