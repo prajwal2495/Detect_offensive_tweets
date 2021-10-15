bb7b3e7188af7065a3357328b807b4b37ade3540
MrinalChaudhari <mission999c@gmail.com>
2021-10-15 11:00:09 -0400

    loaded training and testing data in main method

--- Experiments-Mrinal/Classical_Models.py
+++ Experiments-Mrinal/Classical_Models.py
@@ -4,3 +4,11 @@ def read_data(filename):
     file_content = pd.read_csv(filename)
     return file_content
 
+def main():
+    train_filename = './Data/Marathi_Train.csv'
+    train_data = read_data(train_filename)
+    train_data = train_data[['Tweet', 'Class']]
+
+    test_filename = './Data/Marathi_Test.csv'
+    test_data = read_data(test_filename)
+    test_data = test_data[['Tweet', 'Class']]
