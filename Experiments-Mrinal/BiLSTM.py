2781ef4a87dc7830c258d686dc893e6eac1a8af1
MrinalChaudhari <mission999c@gmail.com>
2021-10-16 10:13:34 -0400

    Splitting training and testing data

--- Experiments-Mrinal/BiLSTM.py
+++ Experiments-Mrinal/BiLSTM.py
@@ -4,3 +4,10 @@ def read_data(filename):
     file_content = pd.read_csv(filename)
     return file_content
 
+train_data = read_data('./Data/Marathi_Train.csv')
+test_data = read_data('./Data/Marathi_Test.csv')
+train_data['Class'] = train_data['Class'].map({'Not Offensive': 1, 'Offensive': 0})
+test_data['Class'] = test_data['Class'].map({'Not Offensive': 1, 'Offensive': 0})
+X_train, X_valid, Y_train, Y_valid = train_data.Tweet, test_data.Tweet, train_data.Class, test_data.Class
+
+type(Y_train[0])
