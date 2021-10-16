9990f8b1f43b7a25f897debb9f46dd5d0c223c44
MrinalChaudhari <mission999c@gmail.com>
2021-10-16 10:36:49 -0400

    Embedded sequence added

--- Experiments-Mrinal/BiLSTM.py
+++ Experiments-Mrinal/BiLSTM.py
@@ -11,9 +11,17 @@ test_data['Class'] = test_data['Class'].map({'Not Offensive': 1, 'Offensive': 0}
 X_train, X_valid, Y_train, Y_valid = train_data.Tweet, test_data.Tweet, train_data.Class, test_data.Class
 
 type(Y_train[0])
-
+EMBED_SIZE = 128
 MAX_FEATURES = 3000
 
 tokenizer = Tokenizer(num_words=MAX_FEATURES, lower=True,split=' ')
 tokenizer.fit_on_texts(train_data['Tweet'].values)
 list_tokenized_train = tokenizer.texts_to_sequences(train_data['Tweet'].values)
+
+X_train = pad_sequences(list_tokenized_train)
+
+X_valid = tokenizer.texts_to_sequences(test_data['Tweet'].values)
+X_valid = pad_sequences(X_valid)
+
+sequence_input = Input(shape=(MAX_LEN,), dtype="int32")
+embedded_sequences = Embedding(MAX_FEATURES, EMBED_SIZE)(sequence_input)
