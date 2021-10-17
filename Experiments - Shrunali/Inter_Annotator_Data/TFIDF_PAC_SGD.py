ff095a98aca4752c0702087bd9ed53089d424d31
ShrunaliPaygude <shrunali.paygude@gmail.com>
2021-10-17 17:05:48 -0400

    PAC implemented- not good results

--- Experiments - Shrunali/Inter_Annotator_Data/TFIDF_PAC_SGD.py
+++ Experiments - Shrunali/Inter_Annotator_Data/TFIDF_PAC_SGD.py
@@ -22,3 +22,32 @@ def tokenize(sentences):
         words.extend(w)
         words = sorted(list(set(words)))
     return words
+
+def TFIDF_PassiveAgressive(train_data, test_data):
+    print("TFIDF + PassiveAgressive")
+    model = make_pipeline(TfidfVectorizer(ngram_range=(1,1)), PassiveAggressiveClassifier(max_iter=100,random_state=0,tol=1e-3))
+    X_train = train_data['Tweet']
+    y_train = train_data['Class']
+
+    X_test = test_data['Tweet']
+    y_test = test_data['Class']
+    model.fit(X_train, y_train)
+    labels = model.predict(X_test)
+    print("Accuracy:", metrics.accuracy_score(y_test, labels) * 100)
+    # takes a lot of time to generate 10 trees and find accuracy
+    # print(cross_val_score(model, X, y, cv=10))
+    cm = confusion_matrix(y_test, labels, train_data['Class'].unique())
+    print("Confusion matrix", cm)
+    print(classification_report(y_test, labels, digits = 4))
+    print("\n\n")
+
+def main():
+    train_filename = './Dataset/Marathi_Train.csv'
+    train_data = read_data(train_filename)
+    train_data = train_data[['Tweet', 'Class']]
+
+    test_filename = './Dataset/Marathi_Test.csv'
+    test_data = read_data(test_filename)
+    test_data = test_data[['Tweet', 'Class']]
+
+    TFIDF_PassiveAgressive(train_data, test_data)
