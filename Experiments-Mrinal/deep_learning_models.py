b0565e808158dac17e86e940e1279cb3d5d47a64
MrinalChaudhari <mission999c@gmail.com>
2021-11-14 19:14:13 -0500

    Added Language Identification model using pycld2

--- Experiments-Mrinal/deep_learning_models.py
+++ Experiments-Mrinal/deep_learning_models.py
@@ -19,10 +19,10 @@ def TFIDF_Multi_Layer_Perceptron(train_data, test_data):
     print("TFIDF + MultiLayer Perceptron")
     model = make_pipeline(TfidfVectorizer(),MLPClassifier(random_state=1, max_iter=100, learning_rate_init=0.001, activation='logistic'))
     X_train = train_data['tweet'].values.astype('U')
-    y_train = train_data['subtask_a'].values.astype('U')
+    y_train = train_data['subtask_b'].values.astype('U')
 
     X_test = test_data['tweet'].values.astype('U')
-    y_test = test_data['subtask_a'].values.astype('U')
+    y_test = test_data['subtask_b'].values.astype('U')
 
     model.fit(X_train, y_train)
     labels = model.predict(X_test)
@@ -43,10 +43,10 @@ def LDA_Multi_Layer_Perceptron(train_data, test_data):
     print("LDA + MultiLayer Perceptron")
     model = make_pipeline(CountVectorizer(ngram_range=(1, 1)),LDA(),MLPClassifier(random_state=1, max_iter=100, learning_rate_init=0.001, activation='logistic'))
     X_train = train_data['tweet'].values.astype('U')
-    y_train = train_data['subtask_a'].values.astype('U')
+    y_train = train_data['subtask_c'].values.astype('U')
 
     X_test = test_data['tweet'].values.astype('U')
-    y_test = test_data['subtask_a'].values.astype('U')
+    y_test = test_data['subtask_c'].values.astype('U')
 
     model.fit(X_train, y_train)
     labels = model.predict(X_test)
@@ -62,16 +62,16 @@ def LDA_Multi_Layer_Perceptron(train_data, test_data):
 def main():
     train_filename = './Data/MOLDV2_Train.csv'
     train_data = read_data(train_filename)
-    train_data = train_data[['tweet', 'subtask_a']]
-    train_data = train_data[train_data['subtask_a'].notna()]
+    train_data = train_data[['tweet', 'subtask_b']]
+    train_data = train_data[train_data['subtask_b'].notna()]
 
     test_filename = './Data/MOLDV2_Test.csv'
     test_data = read_data(test_filename)
-    test_data = test_data[['tweet', 'subtask_a']]
-    test_data = test_data[test_data['subtask_a'].notna()]
+    test_data = test_data[['tweet', 'subtask_b']]
+    test_data = test_data[test_data['subtask_b'].notna()]
 
     TFIDF_Multi_Layer_Perceptron(train_data, test_data)
-    LDA_Multi_Layer_Perceptron(train_data, test_data)
+    #LDA_Multi_Layer_Perceptron(train_data, test_data)
 
 if __name__ == '__main__':
     main()
