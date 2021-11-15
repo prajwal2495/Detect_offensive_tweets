2a548e66de36a2c9b4f09cf4f2e41d1568a1e075
ShrunaliPaygude <shrunali.paygude@gmail.com>
2021-11-14 21:42:03 -0500

    adding some models

--- Experiments - Shrunali/TFIDF_PAC_SGD.py
+++ Experiments - Shrunali/TFIDF_PAC_SGD.py
@@ -62,6 +62,104 @@ def TFIDF_SGDClassifer(train_data, test_data):
     print(classification_report(y_test, labels, digits = 4))
     print("\n\n")
 
+def TFIDF_LogisticRegression(train_data, test_data):
+    print("TFIDF + LogisticRegression")
+    model = make_pipeline(TfidfVectorizer(ngram_range=(1,1)), LogisticRegression(random_state=0))
+    X_train = train_data['Tweet']
+    y_train = train_data['Class']
+
+    X_test = test_data['Tweet']
+    y_test = test_data['Class']
+    model.fit(X_train, y_train)
+    labels = model.predict(X_test)
+    print("Accuracy:", metrics.accuracy_score(y_test, labels) * 100)
+    # takes a lot of time to generate 10 trees and find accuracy
+    # print(cross_val_score(model, X, y, cv=10))
+    cm = confusion_matrix(y_test, labels, train_data['Class'].unique())
+    print("Confusion matrix", cm)
+    print(classification_report(y_test, labels, digits = 4))
+    print("\n\n")
+
+def BOW_PASSIVE_AGGRESSIVE(train_data, test_data):
+    print("BOW + PASSIVE_AGGRESSIVE")
+    model = make_pipeline(CountVectorizer(ngram_range=(1, 1)), PassiveAggressiveClassifier(max_iter=100,random_state=0,tol=1e-3))
+    X_train = train_data['Tweet']
+    y_train = train_data['Class']
+
+    X_test = test_data['Tweet']
+    y_test = test_data['Class']
+    model.fit(X_train, y_train)
+    labels = model.predict(X_test)
+    print("Accuracy:", metrics.accuracy_score(y_test, labels) * 100)
+    # takes a lot of time to generate 10 trees and find accuracy
+    # print(cross_val_score(model, X, y, cv=10))
+    cm = confusion_matrix(y_test, labels, train_data['Class'].unique())
+    print("Confusion matrix", cm)
+    print(classification_report(y_test, labels, digits = 4))
+    print("\n\n")
+
+def BOW_LogisticRegression(train_data, test_data):
+    print("BOW + LogisticRegression")
+    model = make_pipeline(CountVectorizer(ngram_range=(1, 1)), LogisticRegression(random_state=0))
+    X_train = train_data['Tweet']
+    y_train = train_data['Class']
+
+    X_test = test_data['Tweet']
+    y_test = test_data['Class']
+    model.fit(X_train, y_train)
+    labels = model.predict(X_test)
+    print("Accuracy:", metrics.accuracy_score(y_test, labels) * 100)
+    # takes a lot of time to generate 10 trees and find accuracy
+    # print(cross_val_score(model, X, y, cv=10))
+    cm = confusion_matrix(y_test, labels, train_data['Class'].unique())
+    print("Confusion matrix", cm)
+    print(classification_report(y_test, labels, digits = 4))
+    print("\n\n")
+
+def LDA_PASSIVE_AGGRESSIVE(train_data, test_data):
+    print("LDA + PASSIVE_AGGRESSIVE")
+    model = make_pipeline(CountVectorizer(), LDA(), PassiveAggressiveClassifier(max_iter=100,random_state=0,tol=1e-3))
+    X_train = train_data['Tweet']
+    y_train = train_data['Class']
+
+    X_test = test_data['Tweet']
+    y_test = test_data['Class']
+    model.fit(X_train, y_train)
+    labels = model.predict(X_test)
+    print("Accuracy:", metrics.accuracy_score(y_test, labels) * 100)
+    # takes a lot of time to generate 10 trees and find accuracy
+    # print(cross_val_score(model, X, y, cv=10))
+    cm = confusion_matrix(y_test, labels, train_data['Class'].unique())
+    print("Confusion matrix", cm)
+    print(classification_report(y_test, labels, digits = 4))
+    print("\n\n")
+
+def LDA_LogisticRegression(train_data, test_data):
+    print("LDA + LogisticRegression")
+    model = make_pipeline(CountVectorizer(), LDA(), LogisticRegression(random_state=0))
+    X_train = train_data['Tweet']
+    y_train = train_data['Class']
+
+    X_test = test_data['Tweet']
+    y_test = test_data['Class']
+    model.fit(X_train, y_train)
+    labels = model.predict(X_test)
+    print("Accuracy:", metrics.accuracy_score(y_test, labels) * 100)
+    # takes a lot of time to generate 10 trees and find accuracy
+    # print(cross_val_score(model, X, y, cv=10))
+    cm = confusion_matrix(y_test, labels, train_data['Class'].unique())
+    print("Confusion matrix", cm)
+    print(classification_report(y_test, labels, digits = 4))
+    # fpr, tpr = metrics.roc_curve(y_test, labels)
+    # roc_auc=auc(fpr, tpr)
+    decision=(model.predict_proba(X_test)>0.5).astype(int)
+    p=[]
+    for j in range(len(y_test)):
+        p.append(1-decision[j][0])
+    print("AUC_SCORE:", roc_auc_score(y_test,p))
+    # log_fpr, log_tpr, threshold = roc_curve(y_test, p)
+    print("\n\n")
+
 def main():
     train_filename = './Dataset/Marathi_Train.csv'
     train_data = read_data(train_filename)
