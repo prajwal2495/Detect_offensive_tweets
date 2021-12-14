1c3378581cb3276b25471abd4e414e3529bc9aa9
prajwalkrishna2@gmail.com <prajwalkrishna2@gmail.com>
2021-12-14 15:54:31 -0500

    doxygen in progress v1.1

--- main/models/pre_processing.py
+++ main/models/pre_processing.py
@@ -1,8 +1,6 @@
 ##
 """! @brief Defines the Pre-processing script"""
 
-# @section description_pre_processing Data Pre-Processing Description
-#
 
 import re
 import pandas as pd
@@ -10,6 +8,10 @@ import pandas as pd
 
 # pre-processing the data
 def clean_text(row, options):
+    """! A Helps Cleaning all the individual tweets based on the selected options
+    @param row  Individual tweets.
+    @param options A dictionary containing True or False on several options pre-defined for pre-processing purposes.
+    """
 
     if options['lowercase']:
         row = str(row).lower()
@@ -39,6 +41,7 @@ def clean_text(row, options):
         row = re.sub('[+,-,_,=,/,<,>,!,#,$,%,^,&,*,\",:,;,.,' ',\t,\r,\n,\',|]','',row)
     return row
 
+## clean_config is a dictionary that provides several options for pre-processing which helps to make the overall data noise free.
 clean_config = {
     'remove_url': True,
     'remove_mentions': True,
@@ -54,6 +57,10 @@ clean_config = {
 
 
 def demoji(text):
+    """! Helper function demojize is used remove the emojis from the tweets.
+    Emojis does not help in understanding the data better in the perspective of teh model.
+    @param text  Individual tweets.
+    """
     emoji_pattern = re.compile("["
         u"\U0001F600-\U0001F64F"  # emoticons
         u"\U0001F300-\U0001F5FF"  # symbols & pictographs
@@ -67,7 +74,9 @@ def demoji(text):
 
 
 def main(train):
-    # csv file
+    """! The trigger function main takes a parameter train to check whther train needs to pre-processed or test dataset.
+    @param train Boolean value, True mean training dataset is being pre-processed, False means Testing dataset is being pre-processed
+    """
     input_file = None
     if(train):
         input_file = 'Data/Training.csv'
@@ -81,22 +90,22 @@ def main(train):
     dataset_df = dataset_df[["tweet","subtask_a", "subtask_b", "subtask_c"]]
     #, "subtask_a", "subtask_b", "subtask_c"
 
-    #lowe case conversion
+    ##lowe case conversion
     dataset_df['tweet'] = dataset_df['tweet'].str.lower()
 
-    # calling pre-processing function
+    ##calling pre-processing function
     dataset_df['tweet'] = dataset_df['tweet'].apply(clean_text, args=(clean_config,))
 
-    #stripping leading and trailing whitespaces
+    ##stripping leading and trailing whitespaces
     dataset_df['tweet'] = dataset_df['tweet'].str.strip()
 
-    #remove emojis - not working
+    ##remove emojis - not working
     dataset_df.astype(str).apply(lambda x: x.str.encode('ascii', 'ignore').str.decode('ascii'))
 
-    #remove emojis - working
+    ##remove emojis - working
     dataset_df['tweet'] = dataset_df['tweet'].apply( lambda x : demoji(x))
 
-    # convert df to csv
+    ##convert df to csv
     if(train):
         dataset_df.to_csv('./Data/Training_pre_processed.csv',index = False)
         return dataset_df
