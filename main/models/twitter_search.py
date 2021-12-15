af48922ab4949ad13f9a407869646506fd0611a5
prajwalkrishna2@gmail.com <prajwalkrishna2@gmail.com>
2021-12-15 09:46:12 -0500

    doxygen comments on twitter scrapper

--- main/models/twitter_search.py
+++ main/models/twitter_search.py
@@ -33,9 +33,8 @@ Access token secret = fawDcbdZiGfGyU0TcQ6aIXbO8pkXSRgsjMOiGjLQs0paQ
 
 
 def load_api():
-    """! Helper function demojize is used remove the emojis from the tweets.
-    Emojis does not help in understanding the data better in the perspective of the model.
-    @param text  Individual tweets.
+    """! Method used to load tweepy API
+    @:return returns a tweepy API object
     """
 
     consumer_key = 'aAXk7BfNJMIu7MWnGyqY5mnQ6'
@@ -49,9 +48,15 @@ def load_api():
 
 
 def tweet_search(api, query, max_tweets, max_id, since_id, geocode):
-    """ Function that takes in a search string 'query', the maximum
-        number of tweets 'max_tweets', and the minimum (i.e., starting)
-        tweet id. It returns a list of tweepy.models.Status objects. """
+    """! Method used to scrap the twitter for MArathi tweets.
+    @param api Tweepy API object
+    @param query List of Marathi phrases/words used to query the tweepy api to scrap the twitter
+    @param max_tweets Maximum number of tweets to be extracted
+    @param max_id the max_id of all the tweets extracted
+    @param since_id To keep track of teh last tweet that was extracted
+    @param geocode area of search from where the tweets needs to extracted from
+    @:return all the searched tweets and maximum id
+    """
 
     searched_tweets = []
     while len(searched_tweets) < max_tweets:
@@ -76,11 +81,13 @@ def tweet_search(api, query, max_tweets, max_id, since_id, geocode):
 
 
 def get_tweet_id(api, date='', days_ago=100, query='a'):
-    """ Function that gets the ID of a tweet. This ID can then be
-        used as a 'starting point' from which to search. The query is
-        required and has been set to a commonly used word by default.
-        The variable 'days_ago' has been initialized to the maximum
-        amount we are able to search back in time."""
+    """! Method used to scrap the twitter for MArathi tweets.
+    @param api Tweepy API object
+    @param date date of the tweet that was tweeted
+    @param days_ago Max number of days to be scanned
+    @param query List of Marathi phrases/words used to query the tweepy api to scrap the twitter
+    @return the Id of the first tweet
+    """
 
     if date:
         # return an ID from the start of the given day
@@ -98,18 +105,12 @@ def get_tweet_id(api, date='', days_ago=100, query='a'):
         return tweet[0].id
 
 
-def write_tweets(tweets, filename):
-    """ Function that appends tweets to a file. """
-
-    with open(filename, 'a') as f:
-        f.write('{"data": [')
-        for tweet in tweets:
-            json.dump(tweet._json, f)
-            f.write(',\n')
-        f.write(']}')
 
 def write_to_csv(tweets, filename):
-    """ Function that appends tweets to a file. """
+    """! A method used to write all the extracted tweets to a CSV file
+    @param tweets all teh extracted tweets
+    @param filename Filename where the tweets will be written to.
+    """
     csv_file = open(filename,'a')
     csvWriter = csv.writer(csv_file)
     for i in range(len(tweets)):
@@ -117,9 +118,10 @@ def write_to_csv(tweets, filename):
 
 
 def main():
-    """This is a script that continuously searches for tweets
+    """!This is a script that continuously searches for tweets
         that were created over a given number of days. The search
-        dates and search phrase can be changed below. """
+        dates and search phrase can be changed below.
+    """
 
     """search variables: """
     search_phrases = ['म्हणतो', 'म्हनतो', 'बघत', 'सरवात']
